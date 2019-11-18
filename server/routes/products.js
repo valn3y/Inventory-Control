4e497ab0cc51b422817ca1857373a61a01ea4022
Valney <valne.junior@icomp.ufam.edu.br>
2019-11-17 22:46:35 -0400

    Fix some bugs

--- server/routes/products.js
+++ server/routes/products.js
@@ -11,6 +11,7 @@ router.post('/', wrapAsync(async (req, res, next) => {
 }))
 
 router.post('/updateArduino', wrapAsync(async (req, res, next) => {
+    console.log(req.body)
     let action = await Products.findOne({ rfid: req.body.rfid })
     let modified = await Products.findOne({ update: 'modified' })
 
